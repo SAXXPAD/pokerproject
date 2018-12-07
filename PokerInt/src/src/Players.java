d81aebe8da85e1e9abc21aeabe7f9416f2cb1103
SAXXPAD <35582697+SAXXPAD@users.noreply.github.com>
2018-12-07 00:07:39 -0500

    Final submission (beta)
    Final submission (beta)

---
+++ PokerInt/src/src/Players.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Players{
+	
+	Scanner sc = new Scanner(System.in);
+	
+	private int headCount;
+	private String str = sc.next();
+	
+	public int getHeadCount() {
+		
+		return headCount;
+	}
+	
+	public void setHeadCount(int headCount) {
+		
+		this.headCount = headCount;
+		
+	}
+
+	public String getStr() {
+		
+		return str;
+		
+	}
+
+	public void setStr(String str) {
+		
+		this.str = str;
+		
+	}
+	 
+}
---
+++ PokerInt/src/src/RNG.java
@@ -0,0 +1,20 @@
+//A class dedicated to the Math class and different operators
+public class RNG {
+
+	public static void main(String[] args) {
+		
+		int x = 25 - 5 * 4 / 2 - 10 + 4;
+		double w = 5%2;
+			
+		System.out.println(x);
+		
+		System.out.println(w);
+		w += 2.2;
+		System.out.println(w--);
+		System.out.println(w);
+		
+	}
+	
+	
+	
+}
