6a3bfdb74566622e44b4ea38dfdfb305e7d10e24
SAXXPAD <35582697+SAXXPAD@users.noreply.github.com>
2018-12-15 11:20:13 -0500

    Updated SRC w/PSI 1 and PSI 2 near complete
    not finalized

--- src/OtherInts.java
+++ src/OtherInts.java
@@ -1,20 +1,22 @@
 
 public class OtherInts {
-	
-	int number;
-	
-	public OtherInts(int num) {
-	/*this is an example of using a string method where variable declared "palindrome" 
-	 * is placed to place a palindrome to be given a string that is a palindrome to be later evaluated
-	 * and given character lengths */
-	String palindrome = "Dot saw I was Tod";
+
+	private String palindrome = "Dot saw I was Tod";
 	//gives you the length of the character length of the palindrome you established in your
-	int len = palindrome.length();
-	/*this gives the string instance variable "str" the value of whatever num you declare 
-	 * within your class. In this case, num would be equivalent to the length to the letters in palindrome.*/
-	String str = String.valueOf(num);
+	private int len = palindrome.length();
+
+	public void setOtherInts(int len) {
+		
+		this.len = len;
 	
-	System.out.println();
+	}
+	public String getOtherInts() {
+
+	/*this gives the string instance variable "str" the value of whatever len you declare 
+	 * within your class. In this case, len would be equivalent to the length to the letters in palindrome.*/
+	String str = String.valueOf(len);
+	
+	return str;
 	
 	}
 	
@@ -22,4 +24,4 @@ public class OtherInts {
 
 /* Use methods to incorporate your contents in "OtherInts" to then call back the functionality
  *  of your methods through print statements*/
- */
+ 
